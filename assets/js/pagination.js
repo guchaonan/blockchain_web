f0b84b8a9ecadbd6e3a1bcd7835945dea7bd68b8
TlalalaT <a15840732040@gmail.com>
2020-08-24 18:01:08 +0800

    zlz近期修改

--- assets/js/pagination.js
+++ assets/js/pagination.js
@@ -1,214 +1,164 @@
-function firstPage(numCount, columnsCounts, pageCount, pageNum, blockTable, preSpan, firstSpan, nextSpan, lastSpan, pageNumSpan, currPageSpan) {
-    hide(numCount, blockTable);
-    var currPageNum = 1;
-    showCurrPage(currPageSpan,currPageNum);
-    showTotalPage(pageNumSpan,pageNum);
-    for (i = 0; i < pageCount; i++) {
+function firstPage(){
+    hide();
+    currPageNum = 1;
+    showCurrPage(currPageNum);
+    showTotalPage();
+    for(i = 0; i < pageCount; i++){
         blockTable.rows[i].style.display = "";
     }
 
-    firstText(firstSpan);
-    preText(preSpan);
-    if (pageNum != 1) {
-        nextLink(currPageNum, numCount, columnsCounts, pageCount, pageNum, blockTable, preSpan, firstSpan, nextSpan, lastSpan, pageNumSpan, currPageSpan);
-    }
-    else {
-        nextText;
-    }
-    lastLink(numCount, columnsCounts, pageCount, pageNum, blockTable, preSpan, firstSpan, nextSpan, lastSpan, pageNumSpan, currPageSpan);
+    firstText();
+    preText();
+    nextLink();
+    lastLink();
 }
 
-function prePage(currPageNum, numCount, columnsCounts, pageCount, pageNum, blockTable, preSpan, firstSpan, nextSpan, lastSpan, pageNumSpan, currPageSpan) {
-    hide(numCount, blockTable);
+function prePage(){
+    hide();
     currPageNum--;
-    showCurrPage(currPageSpan,currPageNum);
-    showTotalPage(pageNumSpan,pageNum);
-    var firstR = firstRow(currPageNum, pageCount);
+    showCurrPage(currPageNum);
+    showTotalPage();
+    var firstR = firstRow(currPageNum);
     var lastR = lastRow(firstR);
-    for (i = firstR - 1; i < lastR - 1; i++) {
+    for(i = firstR-1; i < lastR-1; i++){
         blockTable.rows[i].style.display = "";
     }
 
-    if (1 == currPageNum) {
-        firstText(firstSpan);
-        preText(preSpan);
-        nextLink(currPageNum, numCount, columnsCounts, pageCount, pageNum, blockTable, preSpan, firstSpan, nextSpan, lastSpan, pageNumSpan, currPageSpan);
-        lastLink(numCount, columnsCounts, pageCount, pageNum, blockTable, preSpan, firstSpan, nextSpan, lastSpan, pageNumSpan, currPageSpan);
-    } else if (pageNum == currPageNum) {
-        preLink(currPageNum, numCount, columnsCounts, pageCount, pageNum, blockTable, preSpan, firstSpan, nextSpan, lastSpan, pageNumSpan, currPageSpan);
-        firstLink(numCount, columnsCounts, pageCount, pageNum, blockTable, preSpan, firstSpan, nextSpan, lastSpan, pageNumSpan, currPageSpan);
-        nextText(nextSpan);
-        lastText(lastSpan);
-    } else {
-        firstLink(numCount, columnsCounts, pageCount, pageNum, blockTable, preSpan, firstSpan, nextSpan, lastSpan, pageNumSpan, currPageSpan);
-        preLink(currPageNum, numCount, columnsCounts, pageCount, pageNum, blockTable, preSpan, firstSpan, nextSpan, lastSpan, pageNumSpan, currPageSpan);
-        nextLink(currPageNum, numCount, columnsCounts, pageCount, pageNum, blockTable, preSpan, firstSpan, nextSpan, lastSpan, pageNumSpan, currPageSpan);
-        lastLink(numCount, columnsCounts, pageCount, pageNum, blockTable, preSpan, firstSpan, nextSpan, lastSpan, pageNumSpan, currPageSpan);
+    if(1 == currPageNum){
+        firstText();
+        preText();
+        nextLink();
+        lastLink();
+    }else if(pageNum == currPageNum){
+        preLink();
+        firstLink();
+        nextText();
+        lastText();
+    }else{
+        firstLink();
+        preLink();
+        nextLink();
+        lastLink();
     }
 
 }
 
-function nextPage(currPageNum, numCount, columnsCounts, pageCount, pageNum, blockTable, preSpan, firstSpan, nextSpan, lastSpan, pageNumSpan, currPageSpan) {
-    hide(numCount, blockTable);
+function nextPage(){
+    hide();
     currPageNum++;
-    showCurrPage(currPageSpan,currPageNum);
-    showTotalPage(pageNumSpan,pageNum);
+    showCurrPage(currPageNum);
+    showTotalPage();
     var firstR = firstRow(currPageNum);
     var lastR = lastRow(firstR);
-    for (i = firstR - 1; i < lastR - 1; i++) {
+    for(i = firstR-1; i < lastR-1; i ++){
         blockTable.rows[i].style.display = "";
     }
 
-    if (1 == currPageNum) {
-        firstText(firstSpan);
-        preText(preSpan);
-        nextLink(currPageNum, numCount, columnsCounts, pageCount, pageNum, blockTable, preSpan, firstSpan, nextSpan, lastSpan, pageNumSpan, currPageSpan);
-        lastLink(numCount, columnsCounts, pageCount, pageNum, blockTable, preSpan, firstSpan, nextSpan, lastSpan, pageNumSpan, currPageSpan);
-    } else if (pageNum == currPageNum) {
-        preLink(currPageNum, numCount, columnsCounts, pageCount, pageNum, blockTable, preSpan, firstSpan, nextSpan, lastSpan, pageNumSpan, currPageSpan);
-        firstLink(numCount, columnsCounts, pageCount, pageNum, blockTable, preSpan, firstSpan, nextSpan, lastSpan, pageNumSpan, currPageSpan);
-        nextText(nextSpan);
-        lastText(lastSpan);
-    } else {
-        firstLink(numCount, columnsCounts, pageCount, pageNum, blockTable, preSpan, firstSpan, nextSpan, lastSpan, pageNumSpan, currPageSpan);
-        preLink(currPageNum, numCount, columnsCounts, pageCount, pageNum, blockTable, preSpan, firstSpan, nextSpan, lastSpan, pageNumSpan, currPageSpan);
-        nextLink(currPageNum, numCount, columnsCounts, pageCount, pageNum, blockTable, preSpan, firstSpan, nextSpan, lastSpan, pageNumSpan, currPageSpan);
-        lastLink(numCount, columnsCounts, pageCount, pageNum, blockTable, preSpan, firstSpan, nextSpan, lastSpan, pageNumSpan, currPageSpan);
+    if(1 == currPageNum){
+        firstText();
+        preText();
+        nextLink();
+        lastLink();
+    }else if(pageNum == currPageNum){
+        preLink();
+        firstLink();
+        nextText();
+        lastText();
+    }else{
+        firstLink();
+        preLink();
+        nextLink();
+        lastLink();
     }
 }
 
-function lastPage(numCount, columnsCounts, pageCount, pageNum, blockTable, preSpan, firstSpan, nextSpan, lastSpan, pageNumSpan, currPageSpan) {
-    hide(numCount, blockTable);
-    var currPageNum = pageNum;
-    showCurrPage(currPageSpan,currPageNum);
-    showTotalPage(pageNumSpan,pageNum);
+function lastPage(){
+    hide();
+    currPageNum = pageNum;
+    showCurrPage(currPageNum);
+    showTotalPage();
     var firstR = firstRow(currPageNum);
-    for (i = firstR - 1; i < numCount; i++) {
+    for(i = firstR-1; i < numCount; i++){
         blockTable.rows[i].style.display = "";
     }
 
-    firstLink(numCount, columnsCounts, pageCount, pageNum, blockTable, preSpan, firstSpan, nextSpan, lastSpan, pageNumSpan, currPageSpan);
-    if (currPageNum != 1) {
-        preLink(currPageNum, numCount, columnsCounts, pageCount, pageNum, blockTable, preSpan, firstSpan, nextSpan, lastSpan, pageNumSpan, currPageSpan);
-    }
-    else {
-        preText(preSpan);
-    }
-    nextText(nextSpan);
-    lastText(lastSpan);
+    firstLink();
+    preLink();
+    nextText();
+    lastText();
 }
 
+function jump_to(){
+    if(currPageSpan.value<1 || currPageSpan.value>pageNum){
+        alert("无此页，请重新输入");
+        showCurrPage(currPageNum);
+        return false;
+    }
+    hide();
+    currPageNum = currPageSpan.value;
+    showCurrPage(currPageNum);
+    showTotalPage();
+    var firstR = firstRow(currPageNum);
+    var lastR = lastRow(firstR);
+    for(i = firstR-1; i < lastR-1; i ++){
+        blockTable.rows[i].style.display = "";
+    }
+    if(currPageNum==1){
+        firstText();
+        preText();
+    }
+    else{
+        firstLink();
+        preLink();
+    }
+    if(currPageNum==pageNum){
+        nextText();
+        lastText();
+    }
+    else{
+        nextLink();
+        lastLink();
+    }
 
+}
 
 // 计算将要显示的页面的首行和尾行
-function firstRow(pageCount, currPageNum) {
-    return pageCount * (currPageNum - 1) + 1;
+function firstRow(currPageNum){
+    return pageCount*(currPageNum - 1) + 1;
 }
 
-function lastRow(firstRow, pageCount) {
+function lastRow(firstRow){
     var lastRow = firstRow + pageCount;
-    if (lastRow > numCount + 1) {
+    if(lastRow > numCount + 1){
         lastRow = numCount + 1;
     }
     return lastRow;
 }
 
-function showCurrPage(currPageSpan,currPageNum) {
-    currPageSpan.value = currPageNum;
+function showCurrPage(cpn){
+    currPageSpan.value = cpn;
 }
 
-function showTotalPage(pageNumSpan,pageNum) {
+function showTotalPage(){
     pageNumSpan.innerHTML = pageNum;
 }
 
 //隐藏所有行
-function hide(numCount, blockTable) {
-    for (var i = 0; i < numCount; i++) {
+function hide(){
+    for(var i = 0; i < numCount; i ++){
         blockTable.rows[i].style.display = "none";
     }
 }
 
 //控制首页等功能的显示与不显示
-function firstLink(numCount, columnsCounts, pageCount, pageNum, blockTable, preSpan, firstSpan, nextSpan, lastSpan, pageNumSpan, currPageSpan) {
-    firstSpan.innerHTML = "<a href='javascript:firstPage(\""+numCount+"\",\""+columnsCounts+"\",\""+pageCount+"\",\""+pageNum+"\",\""+blockTable+"\",\""+preSpan+"\",\""+firstSpan+"\",\""+nextSpan+"\",\""+lastSpan+"\",\""+pageNumSpan+"\",\""+currPageSpan+"\");'>首页</a>";
-}
-function firstText(firstSpan) { firstSpan.innerHTML = "首页"; }
+function firstLink(){firstSpan.innerHTML = "<a href='javascript:firstPage();'>首页</a>";}
+function firstText(){firstSpan.innerHTML = "首页";}
 
-function preLink(currPageNum, numCount, columnsCounts, pageCount, pageNum, blockTable, preSpan, firstSpan, nextSpan, lastSpan, pageNumSpan, currPageSpan) {
-    preSpan.innerHTML = "<a href='javascript:prePage(\""+currPageNum+"\"，\""+numCount+"\",\""+columnsCounts+"\",\""+pageCount+"\",\""+pageNum+"\",\""+blockTable+"\",\""+preSpan+"\",\""+firstSpan+"\",\""+nextSpan+"\",\""+lastSpan+"\",\""+pageNumSpan+"\",\""+currPageSpan+"\");'>上一页</a>";
-}
-function preText(preSpan) { preSpan.innerHTML = "上一页"; }
+function preLink(){preSpan.innerHTML = "<a href='javascript:prePage();'>上一页</a>";}
+function preText(){preSpan.innerHTML = "上一页";}
 
-function nextLink(currPageNum, numCount, columnsCounts, pageCount, pageNum, blockTable, preSpan, firstSpan, nextSpan, lastSpan, pageNumSpan, currPageSpan) {
-    nextSpan.innerHTML = "<a href='javascript:void(0);' onclick:'nextPage(["+currPageNum+"，"+numCount+","+columnsCounts+","+pageCount+","+pageNum+",\""+blockTable+"\",\""+preSpan+"\",\""+firstSpan+"\",\""+nextSpan+"\",\""+lastSpan+"\",\""+pageNumSpan+"\",\""+currPageSpan+"\"]);'>下一页</a>";
-}
-function nextText(nextSpan) { nextSpan.innerHTML = "下一页"; }
+function nextLink(){nextSpan.innerHTML = "<a href='javascript:nextPage();'>下一页</a>";}
+function nextText(){nextSpan.innerHTML = "下一页";}
 
-function lastLink(numCount, columnsCounts, pageCount, pageNum, blockTable, preSpan, firstSpan, nextSpan, lastSpan, pageNumSpan, currPageSpan) {
-    lastSpan.innerHTML = "<a href='javascript:void(0);' onclick='lastPage("+numCount+","+columnsCounts+","+pageCount+","+pageNum+","+blockTable+","+preSpan+","+firstSpan+","+nextSpan+","+lastSpan+","+pageNumSpan+","+currPageSpan+");'>尾页</a>";
-}
-function lastText(lastSpan) { lastSpan.innerHTML = "尾页"; }
-
-function pagin(table, spanPre, spanFirst, spanNext, spanLast, spanTotalPage, spanPageNum) {
-    //全局变量
-    var numCount;       //数据总数量
-    var columnsCounts;  //数据列数量
-    var pageCount;      //每页显示的数量
-    var pageNum;        //总页数
-    var blockTable
-    var preSpan
-    var firstSpan
-    var nextSpan
-    var lastSpan
-    var pageNumSpan
-    var currPageSpan
-    //页面标签变量
-    blockTable = document.getElementById(table);
-    preSpan = document.getElementById(spanPre);
-    firstSpan = document.getElementById(spanFirst);
-    nextSpan = document.getElementById(spanNext);
-    lastSpan = document.getElementById(spanLast);
-    pageNumSpan = document.getElementById(spanTotalPage);
-    currPageSpan = document.getElementById(spanPageNum);
-
-    numCount = document.getElementById(table).rows.length;       //取table的行数作为数据总数量
-    columnsCounts = blockTable.rows[0].cells.length;
-    pageCount = 5;
-
-    pageNum = parseInt(numCount / pageCount);
-    if (0 != numCount % pageCount) {
-        pageNum += 1;
-    }
-    firstPage(numCount, columnsCounts, pageCount, pageNum, blockTable, preSpan, firstSpan, nextSpan, lastSpan, pageNumSpan, currPageSpan);
-}
-
-/*function jump_to(num) {
-    if (num > pageNum || num < 1) {
-        showCurrPage(currPageNum);
-        alert("页码输入错误");
-        return false;
-    }
-    hide();
-    var firstR = firstRow(num);
-    var lastR = lastRow(firstR);
-    for (i = firstR - 1; i < lastR - 1; i++) {
-        blockTable.rows[i].style.display = "";
-    }
-    currPageNum = num;
-    showCurrPage(currPageNum);
-    if (num != 1) {
-        firstLink(); preLink();
-    }
-    else {
-        firstText(); preText();
-    }
-    if (num != pageNum) {
-        nextLink();
-        lastLink();
-    }
-    else {
-        nextText();
-        lastText();
-    }
-
-}*/
+function lastLink(){lastSpan.innerHTML = "<a href='javascript:lastPage();'>尾页</a>";}
+function lastText(){lastSpan.innerHTML = "尾页";}
